0482b65f991049eace07c261c8a88f51ecf7a55c
alen899-my <alenjames899@example.com>
2025-08-31 20:48:25 +0530

    flask complted

--- Forestproject/application.py
+++ Forestproject/application.py
@@ -14,6 +14,41 @@ ridge_model=pickle.load(open("models/model.pkl","rb"))
 standard_scaler=pickle.load(open("models/scaler.pkl","rb"))
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    if request.method=='POST':
+        Temprature=float(request.form.get('Temprature'))
+        RH=float(request.form.get('RH'))
+        Ws=float(request.form.get('Ws'))
+        Rain=float(request.form.get('Rain'))
+        FFMC=float(request.form.get('FFMC'))
+        DMC=float(request.form.get('DMC'))
+        ISI=float(request.form.get('ISI'))
+        Classes=float(request.form.get('Classes'))
+        Region=float(request.form.get('Region'))
+
+        new_data=standard_scaler.transform([[Temprature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=ridge_model.predict(new_data)
+        return render_template("Home.html",results=result[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    else:
+        return render_template("Home.html")
+
+
 if __name__=="__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
